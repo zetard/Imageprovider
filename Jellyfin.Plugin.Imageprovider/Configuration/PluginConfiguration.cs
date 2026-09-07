3460860fb3c3e7cf2c9e9e4c526db876ada56080
Zetard <zetard@gmail.com>
2026-09-07 19:03:52 +0200

    Add automatic fallback to /assets/Movies and /assets/Shows directories

--- Jellyfin.Plugin.Imageprovider/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Imageprovider/Configuration/PluginConfiguration.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using MediaBrowser.Model.Plugins;
 
 namespace Jellyfin.Plugin.Imageprovider.Configuration;
 
 public sealed class PluginConfiguration : BasePluginConfiguration
 {
+    public Dictionary<string, string> PathMappings { get; set; } = new();
 }
